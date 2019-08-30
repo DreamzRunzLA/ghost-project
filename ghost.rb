f5bd5af9a87f16f4cb830f457f85c9648167b98c
DreamzRunzLA <salmaan.a.qadir@gmail.com>
2019-08-29 23:28:52 -0600

    Added methods for playing a game to ghost class

--- ghost.rb
+++ ghost.rb
@@ -14,18 +14,42 @@ class Game
     end
 
     def self.current_player
-
+        @player1
     end
 
     def self.previous_player
-
+        @player2
     end
 
     def self.next_player!
-
+        temp = self.current_player
+        self.current_player = self.previous_player
+        self.previous_player = temp
     end
 
-    
+    def self.take_turn(player)
+        input = gets.chomp
+        if valid_play?(input) == false
+            p "invalid play"
+            self.take_turn(player)
+        else
+            @fragment += string
+            return @fragment
+        end
+    end
+
+    def valid_play?(string)
+        alphabet = ('a'..'z').to_a
+        if alphabet.include?(string) == false
+            p "Sorry, not a valid entry"
+            return false
+        end
+        if @dictionary.values.none? {|v| v.include?(@fragment + string)}
+            p "Sorry, combination will not result in real word"
+            return false
+        end
+        return true
+    end
 
 
 
