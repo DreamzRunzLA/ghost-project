2a45f0a1abcf0ac1cf1e91d79095bbe8cf6de09d
DreamzRunzLA <salmaan.a.qadir@gmail.com>
2019-09-14 13:56:13 -0600

    Finished using solution

--- player.rb
+++ player.rb
@@ -1,20 +1,33 @@
-require_relative 'ghost.rb'
-
 class Player
-    def initialize(name)
-        @name = name
-    end
+  attr_reader :name
 
-    def name
-        @name
-    end
+  def initialize(name)
+    @name = name
+  end
 
-    def guess
-        p "input some shit"
-        return gets.chomp
-    end
+  def alert_invalid_move(letter)
+    puts "#{letter} is not a valid move!"
+    puts "Your guess must be a letter of the alphabet."
+    puts "You must be able to form a word starting with the new fragment.\n"
+  end
 
-    def alert_invalid_guess
-    end
+  def guess(fragment)
+    prompt(fragment)
+    gets.chomp.downcase
+  end
 
+  def inspect
+    "HumanPlayer: #{name}"
+  end
+
+  def to_s
+    name
+  end
+
+  private
+
+  def prompt(fragment)
+    puts "The current fragment is '#{fragment}'."
+    print "Add a letter: "
+  end
 end
